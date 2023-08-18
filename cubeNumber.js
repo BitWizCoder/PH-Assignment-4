40b6b941abc401c0eeea203cfc007b6ed9010a08
مُحَمَّد <mhmdnoman>
2023-08-18 20:49:26 +0600

    added note

--- cubeNumber.js
+++ cubeNumber.js
@@ -1,12 +1,17 @@
 function cubeNumber(number) {
+  // this cube variable (calculation) shouldn't be here. it should be inside else clause.
   let cube = number * number * number;
   if (isNaN(number)) {
-      return "Please give a valid number.";
-} else {
-      return cube;
-  } 
+    return "Please give a valid number.";
+  } else {
+    return cube;
+  }
 }
 
-console.log(cubeNumber(3));
-console.log(cubeNumber(4));
-console.log(cubeNumber(-2));
+// console.log(cubeNumber(3));
+// console.log(cubeNumber(4));
+// console.log(cubeNumber(-2));
+
+
+console.log(cubeNumber(5));
+console.log(cubeNumber('hello'));
